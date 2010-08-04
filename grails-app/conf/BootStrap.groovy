a66c02085abf27253f7c5892a281a8cbcde82087
Shubin Fedor <vedi@linux-svwu.site>
2010-08-04 08:11:13 +0400

    create initial task states

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,17 +1,27 @@
 import dela.Task
 import dela.Subject
 import dela.Account
+import dela.State
 
 class BootStrap {
 
     def dataService
 
 
-     def init = { servletContext ->
-         initAnonymous(servletContext)
-     }
+    def init = { servletContext ->
+        initStates()
+        initAnonymous()
+    }
 
-    void initAnonymous(servletContext) {
+    void initStates() {
+        if (!State.count()) {
+            new State(name: 'active').save()
+            new State(name: 'done').save()
+            new State(name: 'canceled').save()
+        }
+    }
+
+    void initAnonymous() {
         Account account = Account.findByRole(Account.ROLE_ANONYMOUS)
         if (!account) {
             account = new Account(login:'anonymous', email:'none', role:Account.ROLE_ANONYMOUS)
