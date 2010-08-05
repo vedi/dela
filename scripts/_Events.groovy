fa4d76de688d59f20b97540f95ec543bce884237
Shubin Fedor <vedi@linux-svwu.site>
2010-08-06 01:19:17 +0400

    add build number generator script

---
+++ scripts/_Events.groovy
@@ -0,0 +1,21 @@
+import java.text.SimpleDateFormat
+
+eventCompileStart = { kind ->
+	def buildNumber = metadata.'app.buildNumber'
+
+	if (!buildNumber)
+		buildNumber = 1
+	else
+		buildNumber = Integer.valueOf(buildNumber) + 1
+
+	metadata.'app.buildNumber' = buildNumber.toString()
+
+    def formatter = new SimpleDateFormat("dd MM yyyy")
+    def buildDate = formatter.format(new Date(System.currentTimeMillis()))
+    metadata.'app.buildDate' = buildDate
+    metadata.'app.buildProfile' = grailsEnv
+
+    metadata.persist()
+
+	println "**** Compile Starting on Build #${buildNumber}"
+}
