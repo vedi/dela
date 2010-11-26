30fbfef7a0ec76b962aaad8a246c4f6c738f45d7
Fedor Shubin <shubin.fedor@gmail.com>
2010-11-26 18:38:39 +0300

    hack VAADIN validation bug see http://dev.vaadin.com/ticket/3851

--- grails-app/vaadin/dela/ui/common/EntityForm.groovy
+++ grails-app/vaadin/dela/ui/common/EntityForm.groovy
@@ -7,6 +7,9 @@ import com.vaadin.ui.Button.ClickEvent
 import com.vaadin.ui.Button.ClickListener
 import com.vaadin.ui.ComponentContainer
 import com.vaadin.ui.Form
+import com.vaadin.data.Validator
+import com.vaadin.ui.AbstractField
+import com.vaadin.data.Validator.InvalidValueException
 
 /**
  * @author vedi
@@ -50,6 +53,111 @@ class EntityForm extends Form implements Button.ClickListener {
         componentContainer.addComponent(cancelButton)
     }
 
+    /**
+     * TODO: Waiting <a href=http://dev.vaadin.com/ticket/3851>ticket</a>
+     */
+    @Override
+    void validate() {
+        validateField(this);
+        for (final Iterator<Object> i = this.getItemPropertyIds().iterator(); i.hasNext();) {
+            AbstractField field = this.getField(i.next());
+            validateField(field)
+        }
+    }
+
+    /**
+     * TODO: Waiting <a href=http://dev.vaadin.com/ticket/3851>ticket</a>
+     */
+    @Override
+    public boolean isValid() {
+        boolean valid = true;
+        for (final Iterator<Object> i = this.getItemPropertyIds().iterator(); i.hasNext();) {
+            valid &= isFieldValid(this.getField(i.next()));
+        }
+        return valid && isFieldValid(this);
+    }
+
+    Boolean isFieldValid(AbstractField field) {
+        if (field.isEmpty()) {
+            if (field.isRequired()) {
+                return false;
+//            } else {
+//                return true;
+            }
+        }
+
+        if (field.validators == null) {
+            return true;
+        }
+
+        final Object value = field.getValue();
+        for (final Iterator<Validator> i = field.validators.iterator(); i.hasNext();) {
+            if (!(i.next()).isValid(value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void validateField(AbstractField field) {
+        if (field.isEmpty()) {
+            if (field.isRequired()) {
+                throw new Validator.EmptyValueException(field.requiredError);
+//            } else {
+//                return;
+            }
+        }
+
+        // If there is no validator, there can not be any errors
+        if (field.validators == null) {
+            return;
+        }
+
+        // Initialize temps
+        Validator.InvalidValueException firstError = null;
+        LinkedList<InvalidValueException> errors = null;
+        final Object value = field.getValue();
+
+        // Gets all the validation errors
+        for (final Iterator<Validator> i = field.validators.iterator(); i.hasNext();) {
+            try {
+                (i.next()).validate(value);
+            } catch (final Validator.InvalidValueException e) {
+                if (firstError == null) {
+                    firstError = e;
+                } else {
+                    if (errors == null) {
+                        errors = new LinkedList<InvalidValueException>();
+                        errors.add(firstError);
+                    }
+                    errors.add(e);
+                }
+            }
+        }
+
+        // If there were no error
+        if (firstError == null) {
+            return;
+        }
+
+        // If only one error occurred, throw it forwards
+        if (errors == null) {
+            throw firstError;
+        }
+
+        // Creates composite validator
+        final Validator.InvalidValueException[] exceptions = new Validator.InvalidValueException[errors.size()];
+        int index = 0;
+        for (final Iterator<InvalidValueException> i = errors.iterator(); i.hasNext();) {
+            exceptions[index++] = i.next();
+        }
+
+        throw new Validator.InvalidValueException(null, exceptions);
+    }
+
+
+
     void buttonClick(ClickEvent clickEvent) {
         if (clickEvent.button == okButton) {
             commit()
@@ -60,4 +168,6 @@ class EntityForm extends Form implements Button.ClickListener {
 
         window.close()
     }
+
+
 }
