0c34675226b76c40d8e639a0e809ea09cf55227d
Fedor Shubin <shubin.fedor@gmail.com>
2010-12-09 18:55:22 +0300

    fix dataService transactions

--- grails-app/services/dela/DataService.groovy
+++ grails-app/services/dela/DataService.groovy
@@ -1,8 +1,6 @@
 package dela
 
 import dela.context.DataContext
-import com.vaadin.data.Validator.InvalidValueException
-import dela.meta.MetaColumn
 
 /**
  * canInsert, canEdit - is action available in current context, runs twice or more times in UI and in logic
@@ -50,36 +48,40 @@ abstract class DataService<T> implements IDataService<T> {
     @Override
     def save(DataContext dataContext, T domain) {
 
-        boolean isNew = domain.id == null
+        domain.withTransaction {
+            boolean isNew = domain.id == null
 
-        def gainedDomain = isNew?null:gainDomain(dataContext, domain)
+            def gainedDomain = isNew?null:gainDomain(dataContext, domain)
 
-        if (isNew) {
-            assert canInsert(dataContext)
-        } else {
-            assert canEdit(dataContext, gainedDomain)
+            if (isNew) {
+                assert canInsert(dataContext)
+            } else {
+                assert canEdit(dataContext, gainedDomain)
+            }
+
+            // TODO: Translate to VAADIN validation
+            assert canSave(dataContext, gainedDomain, domain)
+
+            if (isNew) {
+                assert domain.save()
+                afterInsert(dataContext, domain)
+            } else {
+                assert domain.validate()
+                domain = domain.merge()
+                afterEdit(dataContext, domain)
+            }
+
+            return domain
         }
-
-        // TODO: Translate to VAADIN validation
-        assert canSave(dataContext, gainedDomain, domain)
-
-        if (isNew) {
-            assert domain.save()
-            afterInsert(dataContext, domain)
-        } else {
-            assert domain.validate()
-            domain = domain.merge()
-            afterEdit(dataContext, domain)
-        }
-
-        return domain
     }
 
     @Override
     def delete(DataContext dataContext, T domain) {
-        assert canDelete(dataContext, gainDomain(dataContext, domain))
-        domain.merge().delete()
-        afterDelete(dataContext, domain)
+        domain.withTransaction {
+            assert canDelete(dataContext, gainDomain(dataContext, domain))
+            domain.merge().delete()
+            afterDelete(dataContext, domain)
+        }
     }
 
     @Override
