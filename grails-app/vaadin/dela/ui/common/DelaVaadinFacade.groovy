565e32567ca2d141c1256fd5266263a739af2fc3
Fedor Shubin <shubin.fedor@gmail.com>
2010-12-20 19:37:22 +0300

    add callback parameter to ui facade methods

--- grails-app/vaadin/dela/ui/common/DelaVaadinFacade.groovy
+++ grails-app/vaadin/dela/ui/common/DelaVaadinFacade.groovy
@@ -14,6 +14,8 @@ import dela.Subject
 import com.vaadin.ui.Button.ClickEvent
 import dela.Setup
 import dela.Account
+import com.vaadin.ui.Window.CloseListener
+import com.vaadin.ui.Window.CloseEvent
 
 /**
  * @author vedi
@@ -29,67 +31,74 @@ class DelaVaadinFacade {
     @Resource
     def messageService
 
-    def createSubjectListButton(sessionContext, application) {
+    def createSubjectListButton(sessionContext, application, callback = null) {
         createActionButton(
                 application,
                 Subject,
                 new ClickListener() {
                     @Override
                     void buttonClick(ClickEvent clickEvent) {
-                        showSubjectListWindow(sessionContext, application.mainWindow)
+                        showSubjectListWindow(sessionContext, application.mainWindow, callback)
                     }
         })
 
     }
 
-    def createOwnSetupButton(sessionContext, application) {
+    def createOwnSetupButton(sessionContext, application, callback = null) {
         createActionButton(
                 application,
                 Setup,
                 new ClickListener() {
                     @Override
                     void buttonClick(ClickEvent clickEvent) {
-                        showOwnSetupWindow(sessionContext, application.mainWindow)
+                        showOwnSetupWindow(sessionContext, application.mainWindow, callback)
                     }
 
                 },
                 'images/skin/blue_config.png')
     }
 
-    def createAccountListButton(sessionContext, application) {
+    def createAccountListButton(sessionContext, application, callback = null) {
         createActionButton(
                 application,
                 Account,
                 new ClickListener() {
                     @Override
                     void buttonClick(ClickEvent clickEvent) {
-                        showAccountListWindow(sessionContext, application.mainWindow)
+                        showAccountListWindow(sessionContext, application.mainWindow, callback)
                     }
 
                 }
         )
     }
 
-    private def showSubjectListWindow(sessionContext, mainWindow) {
-        mainWindow.addWindow(uiTools.createListWindow(
+    private def showSubjectListWindow(sessionContext, mainWindow, callback = null) {
+        def window = uiTools.createListWindow(
                 sessionContext, SubjectService,
                 [
                         tableFactory: {new SubjectTable()},
                         dataViewName: SubjectService.OWN_AND_PUBLIC_DATA_VIEW,
-                ]))
+                ])
+
+        mainWindow.addWindow(window)
+        addCallback(window, callback)
     }
 
-    private def showOwnSetupWindow(sessionContext, mainWindow) {
-        mainWindow.addWindow(new SetupWindow(sessionContext: sessionContext))
+    private def showOwnSetupWindow(sessionContext, mainWindow, callback = null) {
+        def window = new SetupWindow(sessionContext: sessionContext)
+
+        mainWindow.addWindow(window)
+        addCallback(window, callback)
     }
 
-    private def showAccountListWindow(sessionContext, mainWindow) {
-        mainWindow.addWindow(uiTools.createListWindow(
+    private def showAccountListWindow(sessionContext, mainWindow, callback = null) {
+        def window = uiTools.createListWindow(
                 sessionContext, AccountService, [
                         gridFields: ['login', 'email'],
                         formFactory: {new AccountForm()}
-                ]))
-
+                ])
+        mainWindow.addWindow(window)
+        addCallback(window, callback)
     }
 
     private def createActionButton(application, domainClass, ClickListener clickListener,
@@ -103,6 +112,20 @@ class DelaVaadinFacade {
         button.addListener(clickListener)
 
         button
+    }
+
+    private def addCallback(window, callback) {
+        if (callback) {
+            window.addListener(new CloseListener() {
+                @Override
+                void windowClose(CloseEvent e) {
+                    callback()
+                }
+            })
+            true
+        } else {
+            false
+        }
     }
 
 }
